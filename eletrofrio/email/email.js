be6be931fc0416de2886e84cbbf378b410d77a27
HCDS0001 <hcds.rick@gmail.com>
2022-10-28 18:48:56 -0300

    mais 1

--- eletrofrio/email/email.js
+++ eletrofrio/email/email.js
@@ -13,10 +13,12 @@ let idexecutivo = document.getElementById('idexecutivo')
 let classexecutivo = document.getElementsByClassName('classexecutivo')
 let optionexecutivo = document.getElementsByClassName('optionexecutivo')
 let sempedido = document.getElementsByClassName('sempedido')
-let classnumerorepresentante = document.getElementsByClassName('classnumerorepresentante')
-let optionnumerorepresentante = document.getElementsByClassName('optionnumerorepresentante')
-let idrepresentante = document.getElementById('idrepresentante')
+let idnomerepresentante = document.getElementById('idnomerepresentante')
+let idnumerorepresentante = document.getElementById('idnumerorepresentante')
+let idoptionnomerepresentante = document.getElementById('idoptionnomerepresentante')
 let optionnomerepresentante = document.getElementsByClassName('optionnomerepresentante')
+
+
 let iddadosdopedido = document.getElementById('iddadosdopedido')
 let classemail = document.getElementsByClassName('classemail')
 let idpara = document.getElementById('idpara')
@@ -25,12 +27,79 @@ let idassunto = document.getElementById('idassunto')
 let idcorpo = document.getElementById('idcorpo')
 
 let valueattribute = 'value'
+let checkedattribute = 'checked'
 let displayblock = "block"
 let displayinline_block = "inline-block"
 let displaynone = "none"
 let cliquegerar = "(CLIQUE EM GERAR)"
 let problem = "(PROBLEMA)"
 let vazio = "(VAZIO)"
+
+
+let representante = [
+    {numero:'001', nome:'Venda Direta Curitiba', email:'email'},
+    {numero:'004', nome:'Tecnimet', email:'email'},
+    {numero:'011', nome:'FG', email:'email'},
+    {numero:'016', nome:'Lobo', email:'email'},
+    {numero:'018', nome:'Silva Filho', email:'email'},
+    {numero:'024', nome:'Mallet', email:'email'},
+    {numero:'035', nome:'Clecio', email:'email'},
+    {numero:'057', nome:'Refripan', email:'email'},
+    {numero:'062', nome:'M Weidner', email:'email'},
+    {numero:'067', nome:'Tortelli', email:'email'},
+    {numero:'070', nome:'Venda Direta São Paulo', email:'email'},
+    {numero:'077', nome:'Llampe Frio PR', email:'email'},
+    {numero:'082', nome:'HB', email:'email'},
+    {numero:'087', nome:'Baldissera', email:'email'},
+    {numero:'093', nome:'Gurgel', email:'email'},
+    {numero:'094', nome:'Luiz Nachbar', email:'email'},
+    {numero:'096', nome:'Kalliel', email:'email'},
+    {numero:'097', nome:'DE', email:'email'},
+    {numero:'101', nome:'Da Roz', email:'email'},
+    {numero:'102', nome:'Superbrasil', email:'email'},
+    {numero:'106', nome:'Vitoria', email:'email'},
+    {numero:'112', nome:'Ronald', email:'email'},
+    {numero:'114', nome:'Scallet', email:'email'},
+    {numero:'115', nome:'Guide', email:'email'},
+    {numero:'118', nome:'Fast Sul', email:'email'},
+    {numero:'120', nome:'Rio Fast', email:'email'},
+    {numero:'121', nome:'Eletrofast', email:'email'},
+    {numero:'182', nome:'Afonso Araujo', email:'email'},
+    {numero:'187', nome:'ETCO', email:'email'},
+    {numero:'189', nome:'Refriged', email:'email'},
+    {numero:'190', nome:'A&F', email:'email'},
+    {numero:'191', nome:'Produtiva', email:'email'},
+    {numero:'193', nome:'Llampe Frio SC', email:'email'},
+    {numero:'195', nome:'DNC Dealer', email:'email'},
+    {numero:'196', nome:'Brites', email:'email'},
+    {numero:'198', nome:'Euro', email:'email'},
+    {numero:'199', nome:'Erco', email:'email'},
+    {numero:'202', nome:'Eder Mathias', email:'email'},
+    {numero:'203', nome:'HCM', email:'email'},
+    {numero:'207', nome:'Brava', email:'email'},
+    {numero:'213', nome:'Mercato', email:'email'},
+    {numero:'217', nome:'Cold Import', email:'email'},
+    {numero:'218', nome:'Cencosud', email:'email'},
+    {numero:'223', nome:'Hercold', email:'email'},
+    {numero:'226', nome:'EC', email:'email'},
+    {numero:'227', nome:'Edeval', email:'email'},
+    {numero:'228', nome:'JPC', email:'email'},
+    {numero:'229', nome:'Wesley Dal Pozzo', email:'email'},
+    {numero:'230', nome:'Ergosystem', email:'email'},
+    {numero:'231', nome:'SM', email:'email'},
+    {numero:'232', nome:'S&I', email:'email'},
+    {numero:'233', nome:'Control y Sostenibilidad', email:'email'},
+    {numero:'234', nome:'Aarci', email:'email'},
+    {numero:'236', nome:'T&G', email:'email'},
+    {numero:'237', nome:'Rematec', email:'email'},
+    {numero:'238', nome:'FMG', email:'email'},
+    {numero:'239', nome:'PSK', email:'email'},
+]
+
+let quantidadeoptionrepresentante = representante.length
+
+document.body.onload = numerorepresentante
+
 
 function selectfunction() {
     let selectidfunction = idfunction.value
@@ -117,74 +186,28 @@ function selectfunction() {
     iddadosdopedido.style.display = displaynone
 }
 
-function selectanalista() {
-    let selectanalista = classanalista[1].value
-    let quantidadeoptionanalista = 5
-    for (let number = 1; number <= quantidadeoptionanalista; number++) {
-        switch (selectanalista) {
-            case optionanalista[number].value:
-                iddadosdopedido.style.display = displayblock
-                idpara.innerHTML = cliquegerar
-                idcc.innerHTML = cliquegerar
-                idassunto.innerHTML = cliquegerar
-                idcorpo.innerHTML = cliquegerar
-                number = quantidadeoptionanalista
-                break;
-            default:
-                iddadosdopedido.style.display = displayblock
-                idpara.innerHTML = optionanalista[0].value
-                idcc.innerHTML = optionanalista[0].value
-                idassunto.innerHTML = optionanalista[0].value
-                idcorpo.innerHTML = optionanalista[0].value
-                break;
-        }
-    }
-}
-
-function selectajuste() {
-    let selectajuste = classajuste[1].value
-    let quantidadeoptionajuste = 5
-    for (let number = 1; number <= quantidadeoptionajuste; number++) {
-        switch (selectajuste) {
-            case optionajuste[number].value:
-                iddadosdopedido.style.display = displayblock
-                idpara.innerHTML = cliquegerar
-                idcc.innerHTML = cliquegerar
-                idassunto.innerHTML = cliquegerar
-                idcorpo.innerHTML = cliquegerar
-                number = quantidadeoptionajuste
-                break;
-            default:
-                iddadosdopedido.style.display = displayblock
-                idpara.innerHTML = optionajuste[0].value
-                idcc.innerHTML = optionajuste[0].value
-                idassunto.innerHTML = optionajuste[0].value
-                idcorpo.innerHTML = optionajuste[0].value
-                break;
-        }
-    }
-}
-
-function selectplanilhamento() {
-    let selectplanilhamento = classplanilhamento[1].value
-    let quantidadeoptionplanilhamento = 43
-    for (let number = 1; number <= quantidadeoptionplanilhamento; number++) {
-        switch (selectplanilhamento) {
-            case optionplanilhamento[number].value:
-                if (number == 40 || number == 41|| number == 42) {
+function selecttipo(classefunçao, numeroquantidadeoptionfuçao, optionfunçao) {
+    let clique = document.getElementById('clique')
+    clique.click()
+    let selectfunçao = classefunçao[1].value
+    let quantidadeoptionfuçao = numeroquantidadeoptionfuçao
+    for (let number = 1; number <= quantidadeoptionfuçao; number++) {
+        switch (selectfunçao) {
+            case optionfunçao[number].value:
+                if (optionfunçao[number].value == sempedido[0].value || optionfunçao[number].value == sempedido[1].value || optionfunçao[number].value == sempedido[2].value) {
                     iddadosdopedido.style.display = displaynone
                     idpara.innerHTML = cliquegerar
                     idcc.innerHTML = cliquegerar
                     idassunto.innerHTML = cliquegerar
                     idcorpo.innerHTML = cliquegerar
-                    number = quantidadeoptionplanilhamento
-                } else {
+                    number = quantidadeoptionfuçao
+                } else { 
                     iddadosdopedido.style.display = displayblock
                     idpara.innerHTML = cliquegerar
                     idcc.innerHTML = cliquegerar
                     idassunto.innerHTML = cliquegerar
                     idcorpo.innerHTML = cliquegerar
-                    number = quantidadeoptionplanilhamento
+                    number = quantidadeoptionfuçao
                 }
                 break;
             default:
@@ -198,38 +221,27 @@ function selectplanilhamento() {
     }
 }
 
-function selectexecutivo() {
-    let selectexecutivo = classexecutivo[1].value
-    let quantidadeoptionexecutivo = 2
-    for (let number = 1; number <= quantidadeoptionexecutivo; number++) {
-        switch (selectexecutivo) {
-            case optionexecutivo[number].value:
-                iddadosdopedido.style.display = displayblock
+function numerorepresentante() {
+    for (let number = 0; number < quantidadeoptionrepresentante; number++) {
+        let optionrepresentante = document.createElement('option')
+        optionrepresentante.setAttribute('value',representante[number].numero)
+        let textoptionrepresentante = document.createTextNode(representante[number].numero)
+        optionrepresentante.appendChild(textoptionrepresentante)
+        idnumerorepresentante.insertAdjacentElement('beforeend',optionrepresentante)
+    }
+}
+
+function nomerepresentante() {
+    
+    for (let number = 0; number < quantidadeoptionrepresentante; number++) {
+        switch (idnumerorepresentante.value) {
+            case representante[number].numero:
+                idnomerepresentante.setAttribute(valueattribute,representante[number].nome)
+                number = quantidadeoptionrepresentante
                 idpara.innerHTML = cliquegerar
                 idcc.innerHTML = cliquegerar
                 idassunto.innerHTML = cliquegerar
                 idcorpo.innerHTML = cliquegerar
-                number = quantidadeoptionexecutivo
-                break;
-            default:
-                iddadosdopedido.style.display = displayblock
-                idpara.innerHTML = optionexecutivo[0].value
-                idcc.innerHTML = optionexecutivo[0].value
-                idassunto.innerHTML = optionexecutivo[0].value
-                idcorpo.innerHTML = optionexecutivo[0].value
-                break;
-        }
-    }
-}
-
-function selectrepresentante() {
-    let selectclassnumerorepresentante = classnumerorepresentante[1].value
-    let quantidadeoptionrepresentante = 57
-    for (let number = 0; number <= quantidadeoptionrepresentante; number++) {
-        switch (selectclassnumerorepresentante) {
-            case optionnumerorepresentante[number].value:
-                idrepresentante.setAttribute(valueattribute,optionnomerepresentante[number].value)
-                number = quantidadeoptionrepresentante
                 break;
             default:
                 idpara.innerHTML = problem
@@ -242,8 +254,8 @@ function selectrepresentante() {
 }
 
 function limpardadosdopedido() {
-    idrepresentante.setAttribute(valueattribute,optionnomerepresentante[0].value)
-    idrepresentante.value = optionnomerepresentante[0].value
+    idnomerepresentante.setAttribute(valueattribute,representante[0].nome)
+    idnumerorepresentante.value = representante[0].numero
 }
 
 function gerar() {
